29e19590c215f647f1246336d0e2f697b7a29f0c
LhgS <lucas.henrique.gois.santos@gmail.com>
2025-11-14 09:16:39 -0300

    Resetando a CPU

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -10,7 +10,7 @@ struct CPU_internal_data_structure {
     Word PC;        // Program Counter
     Byte SP;        // Stack Counter
 
-    Byte AC;        // Accumulator
+    Byte A;         // Accumulator
     Byte X, Y;      // Resgisters
 
     Byte C : 1;     // Status Flag : Carry Flag
@@ -20,11 +20,20 @@ struct CPU_internal_data_structure {
     Byte B : 1;     // Status Flag : Break Command Flag
     Byte V : 1;     // Status Flag : Overflow Flag
     Byte N : 1;     // Status Flag : Negative Flag
+
+    void Reset() {
+        PC = 0xFFFC;
+        SP = 0x0100;
+        
+        C = Z = I = D = B = V = N = 0;
+
+        A = X = Y = 0;
+    }
 };
 
 int main(void) {
-
     CPU_internal_data_structure cpu;
+    cpu.Reset();
 
     return 0;
 }
